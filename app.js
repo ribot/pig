140975012b5d44d6cc3846d2f398496263063aef
Matt <matt@ribot.co.uk>
2014-04-30 16:54:46 +0100

    Added a response class to encaptulate the response methods

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var bridge = require( './lib/bridge' );
 
 bridge.register( 'request', function( data, res ) {
-  res.send( data );
+  if ( data ) {
+    res.send( data );
+  } else {
+    res.error( 'There was an error' );
+  }
 } );
 
-bridge.send( 'request', 'Request', function( err, data ) {
+bridge.send( 'request', false, function( err, data ) {
   if ( err ) {
     return console.log( 'Error in request: %s', err );
   }
