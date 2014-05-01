140392ad80ac32991280f1aca9f9018ba21107d4
Matt <matt@ribot.co.uk>
2014-05-01 14:50:22 +0100

    Added two buttons to the UI to send the messages

--- app/src/main/java/uk/co/ribot/jsbridge/MainActivity.java
+++ app/src/main/java/uk/co/ribot/jsbridge/MainActivity.java
@@ -3,14 +3,16 @@ package uk.co.ribot.jsbridge;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void buttonMessage1(View v) {
         WebViewWrapper webViewWrapper = ((Application) getApplication()).getWebViewWrapper();
         webViewWrapper.js("message", new WebViewWrapper.Callback() {
             @Override
@@ -19,7 +21,10 @@ public class MainActivity extends Activity {
                 Log.d("MainActivity", "Message: " + response);
             }
         });
+    }
 
+    public void buttonMessage2(View v) {
+        WebViewWrapper webViewWrapper = ((Application) getApplication()).getWebViewWrapper();
         webViewWrapper.js("message2", new WebViewWrapper.Callback() {
             @Override
             public void callback(String error, String response) {
