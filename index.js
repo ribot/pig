a66b3212085c7e6f6f82c8f1d929f7a5e2c991aa
Matt <matt@ribot.co.uk>
2014-05-01 14:18:17 +0100

    Added the code needed to receive and reply to native messages

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-window.bridge = require( './lib/bridge' );
+var bridge = window.bridge = require( './lib/bridge' );
+
+bridge.register( 'message', function( data, res ) {
+  res.send( 'Hello World!' );
+} );
+
+bridge.register( 'message2', function( data, res ) {
+  res.send( 'Hello again!' );
+} );
