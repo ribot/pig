6ea578fde9f7322e767a7a2da717448d9c1fab19
Matt <matt@ribot.co.uk>
2014-05-01 17:38:59 +0100

    Changed the module to work via npm install

--- app.js
+++
@@ -1,9 +0,0 @@
-var bridge = require( './lib/bridge' );
-
-bridge.register( 'message', function( data, res ) {
-  res.send( 'Hello World!' );
-} );
-
-for ( var i = 0; i < 2000; i++ ) {
-  bridge.send( i, 'message', "{}" );
-}
--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var bridge = window.bridge = require( './lib/bridge' );
-
-bridge.register( 'message', function( data, res ) {
-  res.send( 'Hello World!' );
-} );
-
-bridge.register( 'message2', function( data, res ) {
-  res.send( 'Hello again!' );
-} );
-
-bridge.register( 'data', function( data, res ) {
-  var result = data.first + data.second;
-  res.send( result );
-} );
+module.exports = exports = require( './lib/bridge' );
