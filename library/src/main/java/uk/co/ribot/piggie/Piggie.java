05dd9c24df24e9af26f162dd97292c6b19d76b18
Matt <matt@ribot.co.uk>
2014-05-02 14:03:24 +0100

    Changed the interface to support marshalling and unmarshalling

--- library/src/main/java/uk/co/ribot/piggie/Piggie.java
+++ library/src/main/java/uk/co/ribot/piggie/Piggie.java
@@ -1,9 +1,17 @@
 package uk.co.ribot.piggie;
 
 import android.content.Context;
+import android.util.Log;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
 public class Piggie {
+    private static final String TAG = "Piggie";
+
     private static Piggie sPiggie;
+    private final Gson mGson;
+
     public static Piggie get(Context context) {
         if (sPiggie == null) {
             sPiggie = new Piggie(context);
@@ -15,14 +23,43 @@ public class Piggie {
 
     private Piggie(Context context) {
         mWebViewWrapper = new WebViewWrapper(context);
+        mGson = new Gson();
     }
 
     public void send(String path, Callback callback) {
-        send(path, "", callback);
+        send(path, null, callback);
     }
 
-    public void send(String path, String data, Callback callback) {
-        mWebViewWrapper.js(path, data, callback);
+    public void send(String path, Object data, Callback callback) {
+        Class dataClass = null;
+        if (data != null) {
+            dataClass = data.getClass();
+        }
+
+        send(path, data, dataClass, callback);
+    }
+
+    public void send(String path, Object data, Class dataClass, Callback callback) {
+        String json = "";
+        if (data != null) {
+            if (data instanceof String && isAlreadyJson((String) data)) {
+                json = (String) data;
+            } else {
+                json = mGson.toJson(data, dataClass);
+            }
+        }
+        Log.d(TAG, "Json: " + json);
+
+        mWebViewWrapper.js(path, json, callback);
+    }
+
+    private boolean isAlreadyJson(String string) {
+        try {
+            new JsonParser().parse(string);
+            return true;
+        } catch (JsonSyntaxException e) {
+            return false;
+        }
     }
 
     public interface Callback {
